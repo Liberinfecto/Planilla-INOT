79ce6bb0301752bd8b69611e1b2712bdfcfedaa9
Liberinfecto <liberinfecto@gmail.com>
2024-11-28 22:09:11 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,33 +1,4 @@
-const App = () => {
-  const [formData, setFormData] = React.useState({
-    name: '',
-    floor: '',
-    ci: '',
-    age: '',
-    origin: ''
-  });
+const container = document.getElementById('root');
+const root = ReactDOM.createRoot(container);
 
-  const handleInputChange = (e) => {
-    const { name, value } = e.target;
-    setFormData(prevState => ({
-      ...prevState,
-      [name]: value
-    }));
-  };
-
-  return (
-    <div>
-      <h1>Formulario INOT</h1>
-      <input
-        type="text"
-        name="name"
-        value={formData.name}
-        onChange={handleInputChange}
-        placeholder="Nombre"
-      />
-    </div>
-  );
-};
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(React.createElement(App));
+root.render(React.createElement('div', null, 'Hola Mundo'));
