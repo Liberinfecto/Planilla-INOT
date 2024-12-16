c43450651da886639965bac133260b848fe5574b
Liberinfecto <liberinfecto@gmail.com>
2024-12-16 19:50:16 -0300

    Update app.js

--- app.js
+++ app.js
@@ -366,7 +366,7 @@ const styles = {
             marginBottom: '1rem',
             display: 'flex',
             alignItems: 'center',
-            gap: '0.1rem',
+            gap: '0.05rem',
             minWidth: '140px'  // Asegura espacio para el input y la unidad
         },
         smallLabel: {
